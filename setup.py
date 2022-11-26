2b0bd18e048a51f9c5eb3611ccb37eb52dcd55ab
Leinstay <leinstay@gmail.com>
2022-11-26 23:01:32 +0200

    Fyx typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='asciiclip',
-    version='1.0.6',
+    version='1.0.7',
     author='leinstay',
     author_email='leinstay@gmail.com',
     description='',
